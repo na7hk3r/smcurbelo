e9807818bb75bf5d818e37f719c5b00f3f140823
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-04-17 00:22:04 -0300

    Assets y organizacion App e Index

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { React } from 'react'
+import React from 'react';
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero';
 
 function App() {
 
   return (
     <>
+      <Navbar />
+      <div className="app-container">
+        <Hero />
+      </div>
     </>
   )
 }
