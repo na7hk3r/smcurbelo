ecea2f01275fa488ef80bf9aa0ed1cf79390af4a
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-04-17 00:22:25 -0300

    Mejoras en Navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png';
+import ToggleButton from '../ToggleButton/ToggleButton.jsx';
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className='navbar'>
+            <img src={logo} alt="logo" />
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+            <ToggleButton />
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
