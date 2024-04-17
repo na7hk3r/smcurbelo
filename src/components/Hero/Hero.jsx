b8331e0ea3cc833b653f114a15b1c72748ca3e35
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-04-17 00:22:53 -0300

    Hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css';
+import home_img from '../../assets/home_img.png'
+
+const Hero = () => {
+  return (
+    <div className="container">
+        <div className="left-container">
+            <h2>Howdy! 👋</h2>
+            <h1><span>I'm Sergio.</span><br/> I make things for the web.</h1>
+            <p>Uruguay-based web developer. Let me show you my work!</p>
+        </div>
+        <div className="right-container">
+            <img src={home_img} alt="Home Image" />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
