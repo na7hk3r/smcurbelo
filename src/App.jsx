31bfd1999c5e7c841fbb1f15d82f5e8cdf9454c3
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-04-20 21:06:04 -0300

    Correcciones, ajuste de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
+import Skills from './components/Skills/Skills';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <div className="app-container">
         <About />
+        <Skills />
       </div>
     </>
   )
