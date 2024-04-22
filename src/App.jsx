48cea5496fba84735808b79bbb6bf1d9218186ad
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-04-22 16:06:52 -0300

    Update and assets

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
 
@@ -15,7 +17,9 @@ function App() {
         <About />
         <Skills />
         <Projects />
+        <Contact />
       </div>
+      <Footer />
     </>
   )
 }
