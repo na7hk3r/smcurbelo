89a84e79a070aea66b6e931d4ac37ec09d537a33
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-06-29 20:52:37 -0300

    About animaciones iniciales

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,46 @@
 // import React from "react";
 import "./About.css";
 import about_img from "../../assets/about.png";
+import { motion } from "framer-motion";
 
 const About = () => {
+  const initialAnimation = {
+    variants: {
+      initial: {
+        opacity: 0,
+        y: 50,
+      },
+      animate: {
+        opacity: 1,
+        y: 0,
+      },
+    },
+    initial: "initial",
+    whileInView: "animate",
+    viewport: { once: true },
+  };
+
   return (
     <div className="container about-container">
-      <div className="right-container about-left">
+      <motion.div 
+        className="right-container about-left"
+        {...initialAnimation}
+        transition={{ delay: 0.5, type: "spring", stiffness: 100 }}
+      >
         <img src={about_img} alt="AboutMe Image" />
-      </div>
-      <div className="left-container about-right ">
+      </motion.div>
+
+      <motion.div 
+        className="left-container about-right"
+        {...initialAnimation}
+        transition={{ delay: 0.5, type: "spring", stiffness: 100 }}
+      >
         <h2>About Me</h2>
         <h1>Sergio Mathias Curbelo | &lsquo;94</h1>
         <p>I&lsquo;m deeply passionate about technology, learning and arts.</p> 
         <p>I excel in creative and collaborative environments, always prioritizing teamwork, communication, and problem-solving. </p><br />
         <p>I strongly believe that adaptability and a willingness to evolve are invaluable qualities that I can contribute to any organization or project I engage with.</p>
-      </div>
+      </motion.div>
     </div>
   );
 };
