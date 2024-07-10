f99a4629aeedebf8270d6bcaafc5c108fb05c1c2
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-07-09 21:13:33 -0300

    Agrego efecto tilt a cards

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from "react"
+import Tilt from 'react-parallax-tilt'
 import './ProjectCard.css'
 import ProjectsData from './Projects.json';
 import code_icon from '../../assets/code.png';
@@ -8,28 +9,30 @@ const ProjectCard = () => {
   return (
     <>
       {ProjectsData.map(project => (
-        <div key={project.id} className="card-project">
-          <div className="img-container">
-            <img src={project.img} alt="" />
-          </div>
-          <div className="card-info">
-            <h2>{project.title}</h2>
-            <div className="tags">
-              <span>{project.tags.span_1}</span>
-              <span>{project.tags.span_2}</span>
-              <span>{project.tags.span_3}</span>
+        <Tilt key={project.id}>
+          <div className="card-project">
+            <div className="img-container">
+              <img src={project.img} alt="" />
             </div>
-            <p>{project.description}</p>
-            <div className="buttons">
-              <a href={project.href_source} target="_blank" rel="noopener noreferrer">
-                <img src={code_icon} alt="Source Code" />
-              </a>
-              <a href={project.href_source_2} target="_blank" rel="noopener noreferrer">
-                <img src={web_icon} alt="Live Preview" />
-              </a>
+            <div className="card-info">
+              <h2>{project.title}</h2>
+              <div className="tags">
+                <span>{project.tags.span_1}</span>
+                <span>{project.tags.span_2}</span>
+                <span>{project.tags.span_3}</span>
+              </div>
+              <p>{project.description}</p>
+              <div className="buttons">
+                <a href={project.href_source} target="_blank" rel="noopener noreferrer">
+                  <img src={code_icon} alt="Source Code" />
+                </a>
+                <a href={project.href_source_2} target="_blank" rel="noopener noreferrer">
+                  <img src={web_icon} alt="Live Preview" />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        </Tilt>
       ))}
     </>
   );
