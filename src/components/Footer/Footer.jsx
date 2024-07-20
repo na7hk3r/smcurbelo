b3dcf86a23de4921e7be8a132d6ee844bd790584
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-07-20 00:46:14 -0300

    Footer improve

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,48 @@
-import React from 'react'
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
+import logo_sword from "../../assets/logo_sword.png"
 
 const Footer = () => {
-    const date = new Date();
-    const year = date.getFullYear();
+  const date = new Date();
+  const year = date.getFullYear();
   return (
     <div className="footer">
-        <p><span>&copy; {year} </span> 
-        Made with <s>love</s> a keyboard by 
-        <a href="http://github.com/na7hk3r/" target="_blank" rel="noopener noreferrer"> S. M. Curbelo</a>.
-        </p>
-        <ul>
-            <li>
-                <a href="http://github.com/na7hk3r/" 
-                target="_blank" rel="noopener noreferrer">
-                    Github
-                </a>
-            </li>
-            <li>
-                <a href="http://linkedin.com/in/smcurbelo" 
-                target="_blank" rel="noopener noreferrer">
-                        LinkedIn
-                </a>
-            </li>
-        </ul>
+      <ul>
+        <li>
+            <span>
+          {year} Made with <s>love</s> a keyboard by
+          <a
+            href="http://github.com/na7hk3r/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "} S. M. Curbelo
+          </a>
+          .
+          </span>
+        </li>
+        <li>
+          <a
+            href="http://github.com/na7hk3r/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </li>
+        <li>
+          <a
+            href="http://linkedin.com/in/smcurbelo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+      <img src={logo_sword} alt="Sword Nathker Logo" />
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
