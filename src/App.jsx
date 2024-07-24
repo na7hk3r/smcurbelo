e5e97412ac0f1a38c659b4be02f253f799f235ae
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-07-24 02:08:25 -0300

    Vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Skills from './components/Skills/Skills';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <Projects />
         <Contact />
       <Footer />
+      <Analytics />
     </>
   )
 }
